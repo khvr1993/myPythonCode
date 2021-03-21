967f4ec115cb3d4bb38fd60eb79afd03a30b7f92
Harshavardhan Reddy Kundavaram <harshavardhanreddyk@uber.com>
2021-03-21 08:10:57 +0530

    ignore pyenv config

--- Problems/Algorithms/weightedJobScheduling/weightedJobScheduling.py
+++ Problems/Algorithms/weightedJobScheduling/weightedJobScheduling.py
@@ -39,15 +39,14 @@ class Solution:
 
 	def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
 		"""
-			Calculates the optimum Job scheduling and returns the maximum
-			pofit.
+			Calculates the optimum Job scheduling and returns the maximum pofit.
 			1. Weighted Job scheduling is basically selecting the best jobs in a given
 				 set of jobs to maximize the profit.
 			2. To solve this we use the below approach
 			3. First sort the jobs by their finish time
 			4. For every job we take the subsets and determine the maximum profit that can be acheived
-			5. Here since we can store the profit for every job with other job, we can use dynamic programming
-					approach to solve the problem
+			5. Here since we can store the profit for every job with other job,
+			we can use dynamic programming approach to solve the problem
 		"""
 		job_schedule = list(zip(startTime,endTime,profit))
 
