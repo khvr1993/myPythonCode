dc12be0584c2600d4729cc40e5cf0949a83ada17
khvr1993 <khvr.1993@gmail.com>
2021-03-21 16:20:26 +0530

    Longest Palindrome

--- Problems/Solutions/mindeletions/mindeletions.py
+++ Problems/Solutions/mindeletions/mindeletions.py
@@ -4,6 +4,6 @@ class Solution:
     Return minimum number of deletions required to convert a string to a palindrome.
     This can be solved using longest palindrome subsequence.
     Step1: Find the length of the longest palindromic subsequence.
-    Step2: If we delete the characters which are effecting the longest palindromic sequence the
+    Step2: If we delete the characters which are effecting the longest palindromic subsequence the
            string will become palindrome
     """
