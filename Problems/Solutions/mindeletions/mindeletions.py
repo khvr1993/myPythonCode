f8b2f6b8d2a6a4809c186d9675c949075fad87fe
khvr1993 <khvr.1993@gmail.com>
2021-03-21 08:18:32 +0530

    changedd settings

--- Problems/Solutions/mindeletions/mindeletions.py
+++ Problems/Solutions/mindeletions/mindeletions.py
@@ -2,8 +2,8 @@ class Solution:
   def mindeletions(self,s :str) -> int:
     """
     Return minimum number of deletions required to convert a string to a palindrome.
-    This can be solved using longest palindrome sequence.
-    Step1: Find the length of the longest palindromic sequence.
+    This can be solved using longest palindrome subsequence.
+    Step1: Find the length of the longest palindromic subsequence.
     Step2: If we delete the characters which are effecting the longest palindromic sequence the
            string will become palindrome
     """
