152bfb4aadc0a1537e4c5d44307f042aa4801832
khvr1993 <khvr.1993@gmail.com>
2021-04-15 14:58:16 +0530

    reverse linked List

--- Algorithms and Problems/Solutions/ReverseLinkedList/reverseList.py
+++ Algorithms and Problems/Solutions/ReverseLinkedList/reverseList.py
@@ -10,3 +10,24 @@ class Solution:
         https://leetcode.com/problems/reverse-linked-list/
         Given the head of a singly linked list, reverse the list, and return the reversed list.
         """
+        if head == None:
+            return None
+
+        current = head
+        reversed_list = None
+
+        while current:
+            # Store the link to next nodes
+            temp = current.next
+            # Break the link by pointing it to the reversed_list
+            current.next = reversed_list
+
+            # Assigning this will reverse the nodes
+            # As we iterate furthor the list reversal increases
+            reversed_list = current
+
+            # to move to the next node
+            current = temp
+            # print(reversed_list)
+
+        return reversed_list
