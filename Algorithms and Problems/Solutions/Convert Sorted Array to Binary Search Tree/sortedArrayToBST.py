2b1fa1f393c38cc83c440a92b661c5529854a403
Harshavardhan Reddy Kundavaram <harshavardhanreddyk@uber.com>
2021-04-18 13:00:29 +0530

    MAXIMUM Depth

--- Algorithms and Problems/Solutions/Convert Sorted Array to Binary Search Tree/sortedArrayToBST.py
+++ Algorithms and Problems/Solutions/Convert Sorted Array to Binary Search Tree/sortedArrayToBST.py
@@ -7,7 +7,14 @@ from typing import List, TreeNode
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        """
+        https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+        Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+        A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+        """
 
         length_of_the_array = len(nums)
 
